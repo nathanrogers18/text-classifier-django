58ab6773a51e66dddee332f1a56f61416d77e857
mendez6412 <mendez6412@gmail.com>
2016-09-02 13:47:39 -0400

    predictor working

--- classy/views.py
+++ classy/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Classifier
 
 
 def index(request):
-    return render(request, 'classy/index.html', context={})
+    visible_classifiers = Classifier.objects.filter(is_visible=True)
+    classifiers = [(classifier.id, classifier.name) for classifier in visible_classifiers]
+    context = {'classifiers': classifiers}
+    if request.POST:
+        classifier_id = int(request.POST.get('classifier'))
+        return redirect('classifier', pk=classifier_id)
+    else:
+        return render(request, 'classy/index.html', context)
 
 
 def classifier(request, pk):
@@ -11,5 +18,15 @@ def classifier(request, pk):
     corpus = classifier.corpus_set.all()
     labels = {corpi.category for corpi in corpus}
     context = {'classifier': classifier, 'labels': labels}
-
-    return render(request, 'classy/classifier.html', context)
+    if request.POST:
+        print("HELLO")
+        print(request.POST)
+        text = request.POST.get('text')
+        classifier.train()
+        prediction = classifier.predict(text)
+        print(prediction)
+        context['text'] = text
+        context['prediction'] = prediction
+        return render(request, 'classy/classifier.html', context)
+    else:
+        return render(request, 'classy/classifier.html', context)
