0f157a19e716fc8caf6958b9e09f5669fa1727e6
mendez6412 <mendez6412@gmail.com>
2016-09-05 21:49:32 -0400

    fixed index js

--- classy/static/js/index.js
+++ classy/static/js/index.js
@@ -18,10 +18,7 @@ $createClassifier.click(function(){
            'is_visible': true},
     success: function() {
       console.log("WOW");
-<<<<<<< HEAD
       $('#myModal').modal('hide');
-=======
->>>>>>> 1d0e73c694c63d8d38f58f88ea62dffcdb5086dc
     }
 });
 });
